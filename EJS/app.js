45837aa2777c8b0100b1fed826bb25f0e1bfd878
dododoyo <dolphinmulugeta88@gmail.com>
2023-08-15 15:26:36 -0400

    finished to-do with database

--- EJS/app.js
+++ EJS/app.js
@@ -1,48 +1,246 @@
-//jshint esversion:6
-
+/**
+ * Module dependencies.
+ * @const express
+ * @const bodyParser
+ * @const mongoose
+ */
 const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
+// Use body-parser middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// Connect to the local MongoDB database
+mongoose.connect('mongodb://localhost:27017/ToDoDB',{useNewUrlParser:true});
 
-app.get("/", function(req, res) {
+// Define the schema for the 'items' collection
+const itemsSchema = {
+  task:String
+};
 
-const day = date.getDate();
+const listSchema = {
+  task:String,
+  items:[itemsSchema]
+}
 
-  res.render("list", {listTitle: day, newListItems: items});
+// Create a model for the 'items' collection
+const Item = mongoose.model('Item',itemsSchema);
+
+const List = mongoose.model('List',listSchema);
+
+const item1 = new Item({
+  task:'Welcome to your todolist!'
+});
+const item2 = new Item({
+  task:'Hit the + button to add a new item'
+});
+const item3 = new Item({
+  task:'<-- Hit this box to delete an item'
+});
+
+// Define two arrays to hold the items for the 'Today' and 'Work' lists
+let defaultItems = [item1,item2,item3];
+
+
+/**
+ * Add tasks to the database.
+ * @param {Array} theTasks - An array of tasks to be added to the database.
+ */
+function addTasksToDatabase(theTasks) {
+  theTasks.forEach(function(eachTask){
+    const taskToBeAdded = new Item({task: eachTask});
+    taskToBeAdded.save();
+    console.log('Task `'+eachTask+'` was succesfully added.')
+  })
+}
+
+/**
+ * Render the 'Today' list.
+ * @function
+ * @name getTodayList
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
+app.get("/", function (req, res) {
+  Item.find({}).exec().then(function (foundItems) {
+      if(foundItems.length === 0){
+        // only used to add items once so not to get repeated data.
+        Item.insertMany(defaultItems);
+        res.redirect('/');
+      }
+      else{
+        // console.log(foundItems);
+        res.render("list", { listTitle: "Today", newListItems: foundItems });
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+/**
+ * Render a custom list.
+ * @function
+ * @name getCustomList
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
+
+app.get('/:customTaskList',function(req,res){
+    const customTaskList = _.capitalize(req.params.customTaskList); 
+
+    // console.log(customTaskList);
+    // search if the list already exists
+
+    List.findOne({task:customTaskList}).exec().then(function(itemFound){
+      // console.log(itemFound);
+      if (itemFound === null){
+        const list = new List({
+          task: customTaskList,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + customTaskList);
+      }
+      
+      else{
+        res.render("list", { listTitle: itemFound.task, newListItems: itemFound.items});
+      }
+    })
+});
+
+
+/**
+ * Add a new task to the 'Today' or 'Work' list.
+ * @function
+ * @name addTask
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
+app.post("/", function(req, res){
+  const taskName = req.body.newItem;
+  const listType = req.body.list;
+
+  const item = new Item({
+    task:taskName
+  });
+
+  if (listType == 'Today'){
+    item.save()
+    res.redirect('/');
+  }
+  else{
+    List.findOne({task:listType}).exec().then(function(foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/'+listType);
+    })
+  }
 
 });
 
-app.post("/", function(req, res){
+/**
+ * Remove a task from the 'Today' or 'Work' list.
+ * @function
+ * @name deleteTask
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
 
-  const item = req.body.newItem;
+/*app.post('/delete',function(req,res){
+  const checkedItemId = req.body.deleteItem;
+  const listType = req.body.listType;
+  // console.log(taskToDelete);
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
+  if (listType == 'Today'){
+    Item.findByIdAndRemove(checkedItemId).exec().then(function (foundItem) {
+        // console.log('Task `'+ foundItem.task +'` was succesfully removed.');
+        console.log("deleted");
+        res.redirect("/");
+      });
+  }
+  else{
+    //not deleting the ticked task
+    //use for-loop to fix it.
+    List.findOneAndUpdate({task:listType},{$pull:{items:{id_:checkedItemId}}});
+    res.redirect("/" + listType);
+
+    app.post('/delete', function(req, res) {
+  const checkedItemId = req.body.deleteItem;
+  const listType = req.body.listType;
+
+  if (listType === 'Today') {
+    Item.findByIdAndRemove(checkedItemId).exec().then(function(foundItem) {
+      console.log('Task `' + foundItem.task + '` was successfully removed.');
+      res.redirect('/');
+    });
   } else {
-    items.push(item);
-    res.redirect("/");
+    List.findOneAndUpdate({ task: listType }, { $pull: { items: { id_: checkedItemId } } }).exec().then(function(foundList) {
+      console.log('Task `' + checkedItemId + '` was successfully removed from list `' + listType + '`.');
+      res.redirect('/' + listType);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+});
+  }
+});*/
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.deleteItem;
+  const listType = req.body.listType;
+
+  if (listType === "Today") {
+    Item.findByIdAndRemove(checkedItemId)
+      .exec()
+      .then(function (foundItem) {
+        console.log("Task `" + foundItem.task + "` was successfully removed.");
+        res.redirect("/");
+      });
+  } else {
+    List.findOneAndUpdate(
+      { task: listType },
+      { $pull: { items: { id_: checkedItemId } } }
+    )
+      .exec()
+      .then(function (foundList) {
+        console.log(
+          "Task `" +
+            checkedItemId +
+            "` was successfully removed from list `" +
+            listType +
+            "`."
+        );
+        res.redirect("/" + listType);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
-
+/**
+ * Render the 'About' page.
+ * @function
+ * @name getAboutPage
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
 app.get("/about", function(req, res){
   res.render("about");
 });
 
+// Start the server and listen on port 3000
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
